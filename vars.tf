dce7fd500d4d5d23e7e7e9340ac13930f9be126f
AKhilesh <kothapallyakhil123@gmail.com>
2024-01-13 13:10:42 +0530

    updated code

--- vars.tf
+++ vars.tf
@@ -1,9 +1,8 @@
-variable "env" {}
-variable "tags" {}
 variable "vpc_id" {}
 variable "ssh_ingress_cidr" {}
 variable "sg_ingress_cidr" {}
 variable "instance_type" {}
 variable "subnet_ids" {}
 variable "zone_id" {}
-variable "kms_key_id" {}
+variable "env" {}
+variable "tags" {}
