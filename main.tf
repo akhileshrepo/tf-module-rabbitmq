1e302bd66ee56cf5c8482afbaa9f0a923e27b616
AKhilesh <kothapallyakhil123@gmail.com>
2024-03-24 01:33:34 +0530

    updated code

--- main.tf
+++ main.tf
@@ -35,5 +35,5 @@ resource "aws_instance" "main" {
   vpc_security_group_ids = [aws_security_group.main.id]
   subnet_id = var.subnet_ids[0]
   tags        = merge(local.tags, { Name = local.name_prefix })
-  user_data = "${path.module}/userdata.sh"
+  user_data = file("${path.module}/userdata.sh")
 }
