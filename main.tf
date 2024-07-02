f9ed273193a98fa68b8caaa3ecb21a84f261ee6a
AKhilesh <kothapallyakhil123@gmail.com>
2024-07-02 23:33:48 +0530

    updated code

--- main.tf
+++ main.tf
@@ -36,10 +36,7 @@ resource "aws_instance" "main" {
   subnet_id              = var.subnet_ids[0]
   tags                   = merge(local.tags, { Name = local.name_prefix })
   user_data              = file("${path.module}/userdata.sh")
-  root_block_device {
-    encrypted = true
-    kms_key_id = var.kms_key_id
-  }
+
 }
 resource "aws_route53_record" "main" {
   zone_id = var.zone_id
